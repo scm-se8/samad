f60d60c220f5ac8d0ca11bbb03d51222834b9c6a
Sadaqat Ali <49831211+sadaqat1809@users.noreply.github.com>
2021-06-14 10:19:43 +0500

    initilize repo

---
+++ form 1/Form2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form_1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            Form1 f1 = new Form1();
+            this.Hide();
+            f1.Show();
+           // MessageBox.Show("welcome back");
+
+        }
+    }
+}
---
+++ form 1/R_form.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form_1
+{
+    public partial class R_form : Form
+    {
+        public R_form()
+        {
+            InitializeComponent();
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+            rep rp = new rep();
+            crystalReportViewer1.ReportSource = rp;
+            rp.Refresh();
+        }
+    }
+}
